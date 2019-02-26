e215203f67a594be14951a596f854f427fa3570e
João Figueiredo <lucalanca2@hotmail.com>
2019-02-26 17:51:05 +0100

    tentivelly tried to use fetch with preval.macro

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import preval from 'preval.macro';
+
+const USER = preval`
+const fetch = require('isomorphic-unfetch');
+module.exports = (await async function() {
+  return await fetch('https://api.github.com/users/lucalanca')
+    .then(res => res.json())
+    .then(res => {
+      console.log('res', res);
+      return res;
+    });
+ })();
+ `;
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <pre>
+        MACRO RESULT: {JSON.stringify(USER, null, 2)}
+      </pre>
     );
   }
 }
